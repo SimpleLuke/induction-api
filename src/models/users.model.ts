d1abed0d8964bd1e2b7f3f6e4f6e156a69b6616b
Luke Lai <lukelai.dev@gmail.com>
2023-05-07 16:13:54 +0100

    Add test for users model and pass the test

---
+++ src/models/users.model.ts
@@ -0,0 +1,20 @@
+import { Model, ModelObject } from "objection";
+
+export class User extends Model {
+  id!: string;
+  name!: string;
+  email!: string;
+  joined: Date = new Date();
+
+  static tableName = "users"; // database table name
+  static idColumn = "id"; // id column name
+
+  constructor(user?: Partial<User>) {
+    super();
+    if (user) {
+      Object.assign(this, user);
+    }
+  }
+}
+
+export type UserShape = ModelObject<User>;
---
+++ tests/unit-tests/models/users.model.test.ts
@@ -0,0 +1,34 @@
+import { User, UserShape } from "../../../src/models/users.model";
+
+describe("User model", () => {
+  it("should create a new user instance", () => {
+    const user = new User();
+
+    expect(user).toBeDefined();
+    expect(user).toBeInstanceOf(User);
+    expect(user.joined).toBeInstanceOf(Date);
+  });
+
+  it("should create a new user instance with specified values", () => {
+    const userValues: UserShape = {
+      id: "1",
+      name: "John Doe",
+      email: "johndoe@example.com",
+      joined: new Date("2021-01-01"),
+    };
+
+    const user = new User(userValues);
+
+    expect(user).toBeDefined();
+    expect(user).toBeInstanceOf(User);
+    expect(user.id).toBe(userValues.id);
+    expect(user.name).toBe(userValues.name);
+    expect(user.email).toBe(userValues.email);
+    expect(user.joined).toEqual(userValues.joined);
+  });
+
+  it("should have correct table name and id column", () => {
+    expect(User.tableName).toBe("users");
+    expect(User.idColumn).toBe("id");
+  });
+});
